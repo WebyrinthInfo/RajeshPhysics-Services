232ae2dc8a96bc2cd15e215f1a7f631adaf029c6
Webyrinth <99710512+WebyrinthInfo@users.noreply.github.com>
2024-11-25 23:46:59 +0530

    code base

---
+++ src/main/java/com/RajeshPhysics_Services/RajeshPhysicsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.RajeshPhysics_Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajeshPhysicsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajeshPhysicsServicesApplication.class, args);
+	}
+
+}
